f9f2591ca2b55326b8ee77868e33ad7ffba47961
jennn1-jr <jenaraditya123@gmail.com>
2025-05-11 15:24:19 +0700

    initial commit

---
+++ src/main/java/Kelompok5/PBO/Novel_Web/NovelWebApplication.java
@@ -0,0 +1,13 @@
+package Kelompok5.PBO.Novel_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kelompok5/PBO/Novel_Web/NovelWebApplicationTests.java
@@ -0,0 +1,13 @@
+package Kelompok5.PBO.Novel_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
